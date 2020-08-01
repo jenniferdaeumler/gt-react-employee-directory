6b80a440b5db6cb233ffa52dd1266acc1303932a
Jennifer Daeumler <jenniferdaeumler@gmail.com>
2020-08-01 17:50:38 -0500

    comments for future table.

--- src/components/EmployeeTable.js
+++ src/components/EmployeeTable.js
@@ -6,55 +6,48 @@ function EmployeeTable(props) {
       {props.people.map(result => (
         <li className="list-group-item" key={result.id}>
           <img alt="employee" src={result.picture.large} />
-          <li>{result.name.first} {result.name.last}</li>
-          <li>Phone: {result.phone}</li>
-          <li>City: {result.location.city}</li>
-          <li>Email: {result.email}</li>
-          <li>Age: {result.dob.age}</li>
-          <li>Phone: {result.phone}</li>
+          <li className="list-group-item" >{result.name.first} {result.name.last}</li>
+          <li className="list-group-item" >Phone: {result.phone}</li>
+          <li className="list-group-item" >City: {result.location.city}</li>
+          <li className="list-group-item" >Email: {result.email}</li>
+          <li className="list-group-item" >Age: {result.dob.age}</li>
+          <li className="list-group-item" >Phone: {result.phone}</li>
         </li>
       ))}
     </ul>
   );
 }
 
-
 // function EmployeeTable(props) {
-//     return (
-//         <table class="table">
-//         {props.people.map(result => (
-//   <thead>
-//     <tr>
-//       <th scope="col">#</th>
-//       <th scope="col">First</th>
-//       <th scope="col">Last</th>
-//       <th scope="col">Handle</th>
-//     </tr>
-//   </thead>
-//   <tbody>
-//     <tr>
-//       <th scope="row">1</th>
-//       <td>Mark</td>
-//       <td>Otto</td>
-//       <td>@mdo</td>
-//     </tr>
-//     <tr>
-//       <th scope="row">2</th>
-//       <td>Jacob</td>
-//       <td>Thornton</td>
-//       <td>@fat</td>
-//     </tr>
-//     <tr>
-//       <th scope="row">3</th>
-//       <td>Larry</td>
-//       <td>the Bird</td>
-//       <td>@twitter</td>
-//     </tr>
-//   </tbody>
-// </table>
-//         ))}
-//     );
-// }
+// return (
+    
+//     <div className="container">
+//       <table class="table" key={result.id}>
+//         <thead>
+//           <tr>
+//             <th scope="col">ID Picture</th>
+//             <th scope="col">Name</th>
+//             <th scope="col">Email</th>
+//             <th scope="col">Phone</th>
+//           </tr>
+//         </thead>
+//         <tbody>
+//           {/* we need a row with all the information of each employee */}
+//           <tr>
+//             <td>
+//               <img src={result.picture.large} alt={result.firstName}></img>
+//             </td>
+//             <td>
+//             {result.firstName} {result.lastName}
+//             </td>
+//             <td>{result.email}</td>
+//             <td>{result.phone}</td>
+//           </tr>
+//         </tbody>
+//       </table>
+//     </div>
 
+// )
+// }
 
 export default EmployeeTable;
