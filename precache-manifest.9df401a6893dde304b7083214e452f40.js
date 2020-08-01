f04a2d89881647a67217b94e122aebd516e81465
Jennifer Daeumler <jenniferdaeumler@gmail.com>
2020-08-01 09:18:45 -0500

    Updates

---
+++ precache-manifest.9df401a6893dde304b7083214e452f40.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f5ccc4fbce97b0e94e23d6588dbd43c",
+    "url": "/gt-react-employee-directory/index.html"
+  },
+  {
+    "revision": "b58192c4422717f7bc91",
+    "url": "/gt-react-employee-directory/static/js/2.8cb942c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gt-react-employee-directory/static/js/2.8cb942c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24249855b8bdfa832113",
+    "url": "/gt-react-employee-directory/static/js/main.acf6aa61.chunk.js"
+  },
+  {
+    "revision": "507f6b160a865d2a9721",
+    "url": "/gt-react-employee-directory/static/js/runtime-main.0a0b18e8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/gt-react-employee-directory/precache-manifest.a4003e2eb8ebf4c02b21a07ca3c22f1a.js"
+  "/gt-react-employee-directory/precache-manifest.9df401a6893dde304b7083214e452f40.js"
 );
 
 self.addEventListener('message', (event) => {
